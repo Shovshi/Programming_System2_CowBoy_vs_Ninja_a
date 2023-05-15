3f16b4bf5bdcd7b00ec3d7b631c5c30118d63046
shoval <shovalnh12@gmail.com>
2023-05-15 14:36:50 +0300

    initial commit

---
+++ sources/Point.cpp
@@ -0,0 +1,14 @@
+#include "Point.hpp"
+#include <cmath>
+
+namespace ariel
+{
+
+    // Constructors
+    Point::Point(double a , double b)
+    {
+        this->a = a;
+        this->b = b;
+    }
+
+}
---
+++ sources/Point.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+namespace ariel
+{
+    class Point{
+    
+    private:
+    double a;
+    double b;
+
+    public:
+    Point(double a , double b);
+    };
+}
